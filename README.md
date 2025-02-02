ee36cbeaf773b3be9338eb7bb4888195709ba79e
thisisprabhat <prabhat99998@gmail.com>
2025-02-02 20:59:12 +0530

    docs and command created

--- README.md
+++ README.md
@@ -1,2 +1,106 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+# Templify
+
+A cli app built using dart, that creates a module or sub module from a template.
+- It speeds up the development eliminating to copy paste similar modules or portion of code over and over again
+- We can templify similar controllers or ui part in our code and enjoy
+
+## List of commands
+templify has three commands
+- base command
+- config
+- create
+
+### templify
+```
+
+-v, --version      Prints the version of the application.
+
+-h, --help         It shows all the available commands, 
+
+-t, --template     It shows instructions to create template module.
+
+```
+
+
+### templify config
+```
+
+-d, --dir    The path to the directory where the template will be stored.
+
+-n, --name         Name of the template module.
+
+```
+
+
+### templify create 
+```
+
+-n, --name         Name of the module to be created.
+
+-p, --path         Path to the directory where the module will be created.
+
+```
+
+## Template creation guide
+
+> templateDefaultName = `testModule`
+1. Naming files
+    * test_module_model.dart
+    * test_module_controller.dart
+
+2. Naming variables, functions and class
+    * testModuleStatus
+    * fetchTestModuleData()
+    * class TestModuleModel()
+3. Using Title and text as
+    * test module
+    * Test Module
+    * test-module
+
+4. Structure folder structure
+    * root folder should have name of the template
+    * Inside the root folder we should have all the folders and files
+5. In the root template directory we can have a file named `name.txt` and inside it we can have name of the template which will be overridden from global `templateDefaultName`<br>
+    > name.txt
+    ```
+    testModule
+    ```
+5. You may also add `instructions.md` file to be printed after module creation
+    > instructions.md
+    ```
+    ## Copy TestRepo().init() to main() file
+    '''
+    void main(){
+        TestRepo().init();
+        ...
+    }
+    '''
+
+    ....
+    .....
+    ```
+
+## Example template structure
+```ts
+templateName
+├── components
+│   ├── test_card.dart
+│   ├── test_details_info_page.dart
+│   ├── test_details_side.dart
+│   └── test_mobile_view.dart
+├── controllers
+│   ├── add_update_test_provider.dart
+│   └── test_details_provider.dart
+├── data
+│   ├── dummy_test_data.dart
+│   ├── models
+│   │   └── test_model.dart
+│   └── repositories
+│       ├── test_dummy_repo.dart
+│       └── test_remote_repo.dart
+└── views
+    ├── add_update_test.dart
+    ├── test_details.dart
+    └── test_screen.dart
+```
+
--- bin/templify.dart
+++ bin/templify.dart
@@ -1,12 +1,103 @@
+import 'dart:io';
+
 import 'package:args/args.dart';
 import 'package:colored_log/colored_log.dart';
-import 'package:templify/templify.dart';
 
-void main(List<String> arguments) {
-  var parser = ArgParser();
-  var command = parser.addCommand('path');
+void main(List<String> arguments) async {
+  try {
+    ArgParser parser = ArgParser();
+    const appVersion = '1.0.0';
 
-  // final templify = Templify();
-  // templify.operation();
-  ColoredLog.yellow(arguments, name: 'Arguments');
+    //! Flag to print the version of the application ~~~~
+    parser.addFlag(
+      'version',
+      abbr: 'v',
+      help: 'Prints the version of the application.',
+      negatable: false,
+    );
+
+    parser.addFlag(
+      'help',
+      abbr: 'h',
+      help: 'It shows all the available commands and options',
+    );
+
+    parser.addFlag(
+      'template',
+      abbr: 't',
+      help: 'It shows instructions to create template module.',
+    );
+
+    //! Command to open the template directory ~~~~~~~~
+    parser.addCommand('open');
+    //! Command to reset the config ~~~~~~~~~~~~~~~~~~~
+    parser.addCommand('reset');
+
+    //! Command to configure the template directory ~~~
+    ArgParser configParser = ArgParser();
+    //? Configuring template directory
+    configParser.addOption(
+      'dir',
+      abbr: 'd',
+      help: 'The path to the directory where the template will be stored.',
+    );
+    //? Configuring template module name
+    configParser.addOption(
+      'name',
+      abbr: 'n',
+      help: 'Name of the template module.',
+    );
+    parser.addCommand('config', configParser);
+    //? __________________________________________________
+
+    //! Command to create a new module ~~~~~~~~~~~~~~~~~~~
+    ArgParser createParser = ArgParser();
+    createParser.addOption(
+      'name',
+      abbr: 'n',
+      help: 'Name of the module to be created.',
+    );
+
+    createParser.addOption(
+      'path',
+      abbr: 'p',
+      help: 'Path to the directory where the module will be created.',
+    );
+
+    parser.addCommand('create', createParser);
+
+    ArgResults argResults = parser.parse(arguments);
+
+    final bool isOpenCommand = argResults.command?.name == 'open';
+
+    // ColoredLog(argResults.options, name: 'name');
+    // ColoredLog(argResults.command?.name, name: 'Sub Command');
+    // ColoredLog(argResults['version'], name: 'version');
+    // if (argResults['version'] == true) {
+    ColoredLog.green('My CLI Project version: $appVersion');
+    // } else if (argResults.command?.option('name') != null) {
+    //   ColoredLog(argResults['name'], name: 'name Option');
+    // }
+    if (isOpenCommand) {
+      ColoredLog.green('Opening the current directory...');
+      //! Open the current directory
+      await Process.run('open', ['.']);
+    }
+
+    ColoredLog(argResults, name: 'Commands');
+
+    ColoredLog(argResults.command?.name, name: 'Command');
+
+    ColoredLog.yellow(arguments, name: 'Arguments');
+    ColoredLog.yellow('\ntemplify');
+    ColoredLog.green(parser.usage);
+    ColoredLog.yellow('\ntemplify config');
+    ColoredLog.green(configParser.usage);
+    ColoredLog.yellow('\ntemplify create');
+    ColoredLog.green(createParser.usage);
+  } on FormatException catch (e) {
+    ColoredLog.red(e.message, name: 'Invalid', style: LogStyle.italicized);
+  } catch (e) {
+    ColoredLog.red('An error occurred');
+  }
 }
